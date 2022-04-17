40c0cefacb92df45d6db216d7176ce8cd7a900f0
Vid Kreča <34378985+VidKreca@users.noreply.github.com>
2022-04-17 12:51:06 +0200

    [server] Added pre-defined colors

--- packages/backend/src/canvas/canvas.service.ts
+++ packages/backend/src/canvas/canvas.service.ts
@@ -2,6 +2,13 @@ import { Injectable } from '@nestjs/common';
 import { Color, DrawMessage } from '../interfaces/Messages';
 
 const DIMENSIONS = { width: 100, height: 100 };
+const COLORS = [
+  [0, 0, 0],
+  [255, 255, 255],
+  [255, 0, 0],
+  [0, 255, 0],
+  [0, 0, 255],
+];
 
 @Injectable()
 export class CanvasService {
@@ -17,7 +24,10 @@ export class CanvasService {
   }
 
   place(data: DrawMessage): boolean {
-    if (!Array.isArray(data.color) || data.color.length !== 3) return false;
+    if (!this.isValidColor(data.color)) return false;
+
+    if (data.x < 0 || data.x > DIMENSIONS.width - 1) return false;
+    if (data.y < 0 || data.y > DIMENSIONS.height - 1) return false;
 
     this.canvas[data.x][data.y] = data.color;
     this.history.push(data);
@@ -25,8 +35,19 @@ export class CanvasService {
     return true;
   }
 
+  isValidColor(color): boolean {
+    if (!Array.isArray(color) || color.length !== 3) return false;
+    if (!COLORS.find((x) => JSON.stringify(x) === JSON.stringify(color)))
+      return false;
+    return true;
+  }
+
   getConfig() {
-    return { width: DIMENSIONS.width, height: DIMENSIONS.height };
+    return {
+      width: DIMENSIONS.width,
+      height: DIMENSIONS.height,
+      colors: COLORS,
+    };
   }
 
   getCanvas() {
