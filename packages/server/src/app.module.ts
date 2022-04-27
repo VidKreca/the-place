5cc8fa6553cc1a1c9349ec6d2ea588d90a83d4c1
Vid Kreča <34378985+VidKreca@users.noreply.github.com>
2022-04-27 16:10:16 +0200

    [server] Resolved dependency issues

--- packages/server/src/app.module.ts
+++ packages/server/src/app.module.ts
@@ -2,17 +2,11 @@ import { Module } from '@nestjs/common';
 import { PlaceModule } from './socket/place.module';
 import { AdminModule } from './admin/admin.module';
 import { MongooseModule } from '@nestjs/mongoose';
-import { TimeoutModule } from './timeout/timeout.module';
-import { CanvasModule } from './canvas/canvas.module';
-import { ConfigModule } from './config/config.module';
 
 @Module({
   imports: [
-    PlaceModule,
     MongooseModule.forRoot('mongodb://localhost/nest'),
-    TimeoutModule,
-    CanvasModule,
-    ConfigModule,
+    PlaceModule,
     AdminModule,
   ],
 })
