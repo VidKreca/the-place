0be6030b7a944104899448bf47f23614c71bb3b1
Vid Kreča <34378985+VidKreca@users.noreply.github.com>
2022-04-27 16:27:15 +0200

    [server] Moved mongodb config to separate JSON file

--- packages/server/src/app.module.ts
+++ packages/server/src/app.module.ts
@@ -2,10 +2,13 @@ import { Module } from '@nestjs/common';
 import { PlaceModule } from './socket/place.module';
 import { AdminModule } from './admin/admin.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import config from './config.json';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost/nest'),
+    MongooseModule.forRoot(config.mongodb.url, {
+      dbName: config.mongodb.database_name,
+    }),
     PlaceModule,
     AdminModule,
   ],
